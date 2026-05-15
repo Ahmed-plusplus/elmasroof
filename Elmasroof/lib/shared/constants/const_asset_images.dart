5b50733e0d13d5c465260a70cc6613fd066017d2
ahmed mostafa <ahmed8576498@gmail.com>
2026-05-16 01:25:28 +0300

    add image assets to home screen

--- Elmasroof/lib/shared/constants/const_asset_images.dart
+++ Elmasroof/lib/shared/constants/const_asset_images.dart
@@ -1,6 +1,9 @@
 enum ConstAssetImages {
 
-  balance('assets/images/balance.jpeg');
+  balance('assets/images/balance.jpeg'),
+  coinsBank('assets/images/coins_bank.svg'),
+  saveMoney('assets/images/save_money.jpeg'),
+  richChildren('assets/images/rich_children.jpeg');
 
   final String path;
 
